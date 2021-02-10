9ee9a8b7d3861556de380738464e3ee4ce75eff0
lothrazar <samson.bassett@gmail.com>
2021-02-09 22:45:40 -0800

    auto plant fixed placing invalid, add config file

--- src/main/java/com/lothrazar/autoplant/config/ConfigManager.java
+++ src/main/java/com/lothrazar/autoplant/config/ConfigManager.java
@@ -3,24 +3,34 @@ package com.lothrazar.autoplant.config;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
 import com.lothrazar.autoplant.APMod;
+import java.util.Arrays;
+import java.util.List;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 public class ConfigManager {
 
-  private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
-  private static ForgeConfigSpec CFG;
-  public static BooleanValue DOPLANTING;
+  private static final ForgeConfigSpec.Builder CFG = new ForgeConfigSpec.Builder();
+  private static final List<String> DFLT = Arrays.asList(new String[] {
+      "minecraft:grass"
+  });
+  private static ForgeConfigSpec COMMON_CONFIG;
+  public static BooleanValue DOSAPLINGS;
+  public static ConfigValue<List<String>> DOTHESEBLOCKS;
   static {
     initConfig();
   }
 
   private static void initConfig() {
-    BUILDER.comment("General settings").push(APMod.MODID);
-    DOPLANTING = BUILDER.comment("do it").define("doPlanting", true);
-    BUILDER.pop();
-    CFG = BUILDER.build();
+    CFG.comment("General settings").push(APMod.MODID);
+    //
+    DOSAPLINGS = CFG.comment("Planting saplings allowed").define("plantAllSaplings", true);
+    DOTHESEBLOCKS = CFG.comment("Extra blocks to auto plant").define("extras", DFLT);
+    //
+    CFG.pop();
+    COMMON_CONFIG = CFG.build();
   }
 
   public static void setup() {
@@ -30,6 +40,6 @@ public class ConfigManager {
         .writingMode(WritingMode.REPLACE)
         .build();
     configData.load();
-    CFG.setConfig(configData);
+    COMMON_CONFIG.setConfig(configData);
   }
 }
--- src/main/java/com/lothrazar/autoplant/event/ItemEvents.java
+++ src/main/java/com/lothrazar/autoplant/event/ItemEvents.java
@@ -1,10 +1,11 @@
 package com.lothrazar.autoplant.event;
 
-import com.lothrazar.autoplant.APMod;
+import com.lothrazar.autoplant.UtilString;
 import com.lothrazar.autoplant.config.ConfigManager;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.math.BlockPos;
@@ -13,28 +14,52 @@ import net.minecraftforge.event.entity.item.ItemExpireEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ItemEvents {
+  //  @SubscribeEvent
+  //  public void onItemTossEvent(ItemTossEvent event) {
+  //    APMod.LOGGER.info("lfiespan hack " + event.getEntityItem());
+  //    if (event.getPlayer().isCreative()) {
+  //      event.getEntityItem().lifespan = 50;
+  //    }
+  //  }
 
   @SubscribeEvent
   public void onItemExpireEvent(ItemExpireEvent event) {
-    if (ConfigManager.DOPLANTING.get() == false
+    if (ConfigManager.DOSAPLINGS.get() == false
         || event.getEntityItem().isAlive() == false) {
       return;
     }
     ItemEntity item = event.getEntityItem();
     ItemStack itemstack = item.getItem();
-    System.out.println(event.getExtraLife() + "; plantable? " + itemstack);
-    APMod.LOGGER.info("plantable? " + itemstack.getItem().getTags());
-    World world = event.getEntity().world;
-    BlockPos pos = event.getEntity().getPosition();
-    if (world.isAirBlock(pos) &&
-        itemstack.getItem().isIn(ItemTags.SAPLINGS)) {
+    World world = item.world;
+    BlockPos pos = item.getPosition();
+    if (!world.isAirBlock(pos)) {
+      return;
+    }
+    if (this.isPlantable(itemstack.getItem())) {
       //sapling tags yes
-      Block b = Block.getBlockFromItem(itemstack.getItem());
-      if (b != null && b != Blocks.AIR) {
-        if (world.setBlockState(item.getPosition(), b.getDefaultState())) {
+      Block block = Block.getBlockFromItem(itemstack.getItem());
+      if (block != null && block != Blocks.AIR
+          && block.getDefaultState().isValidPosition(world, pos)
+          && world.setBlockState(pos, block.getDefaultState())) {
+        itemstack.shrink(1);
+        event.getEntityItem().setItem(itemstack);
+        if (event.getEntityItem().getItem().isEmpty()) {
           event.getEntity().remove();
+        }
+        else { // not really needed
+          //increase life
+          event.setExtraLife(3000);
+          event.setCanceled(true);
+          //            event.getEntityItem().lifespan = 6000;
         }
       }
     }
+  }
+
+  private boolean isPlantable(Item item) {
+    if (ConfigManager.DOSAPLINGS.get() && item.isIn(ItemTags.SAPLINGS)) {
+      return true;
+    }
+    return UtilString.isInList(ConfigManager.DOTHESEBLOCKS.get(), item.getRegistryName());
   }
 }
