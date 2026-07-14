5d10daafa21f3743feb7d03f0cc4d51a853aeb13
Sam <lothrazar@hotmail.com>
2026-07-13 19:53:09 -0700

    Fix config extra errors on blank entries

--- src/main/java/com/lothrazar/autoplant/config/ConfigAutoPlant.java
+++ src/main/java/com/lothrazar/autoplant/config/ConfigAutoPlant.java
@@ -7,16 +7,16 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class ConfigAutoPlant  {
 
-  private static final List<String> DFLT = Arrays.asList("minecraft:grass");
+  private static final List<String> DFLT = Arrays.asList("minecraft:short_grass");
   public static final ModConfigSpec CONFIG;
   public static ModConfigSpec.BooleanValue DOSAPLINGS;
-  public static ModConfigSpec.ConfigValue<List<String>> DOTHESEBLOCKS;
+  public static ModConfigSpec.ConfigValue<List<? extends String>> DOTHESEBLOCKS;
   static {
     final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     BUILDER.comment("General settings").push(APMod.MODID);
     //
     DOSAPLINGS = BUILDER.comment("Planting saplings allowed").define("plantAllSaplings", true);
-    DOTHESEBLOCKS = BUILDER.comment("Extra blocks to auto plant").define("extras", DFLT);
+    DOTHESEBLOCKS = BUILDER.comment("Extra blocks to auto plant").defineListAllowEmpty("extras",DFLT,()->"",o->o instanceof String);
     //
     BUILDER.pop();
     CONFIG = BUILDER.build();
